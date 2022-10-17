34bc8c46ba2423042efbcad78b30e90a01b9a448
Shreyas4895 <jshreyas11@gmail.com>
2022-10-17 17:07:20 +0530

    React swiper

---
+++ src/components/swiperCard.js
@@ -0,0 +1,65 @@
+import { swiperCard } from "../utils/mockData";
+import "./card.css";
+import { Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const SwiperCard = () => {
+  return (
+    <div className="totalContainer">
+      <div className="headingContainer">
+        <h2>What the press has to say about us</h2>
+      </div>
+      <div className="swiperContainer">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={60}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            800: {
+              slidesPerView: 2,
+              spaceBetween: 50,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <div className="outer">
+            {swiperCard.map((item) => (
+              <SwiperSlide>
+                <div className="box">
+                  <div className="imageContainer">
+                    <img
+                      src={item.image}
+                      alt=""
+                      style={{
+                        width: "140px",
+                        textAlign: "center!important",
+                      }}
+                    />
+                  </div>
+                  <span className="block"></span>
+                  <div className="cardInside">
+                    <h4 style={{ margin: 0 }}>{item.heading}</h4>
+                    <p>"{item.para}"</p>
+                  </div>
+                  <p className="viewStory">View Story</p>
+                  <div className="backgroundGradient"></div>
+                  <div className="whiteBackground"></div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </div>
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+export default SwiperCard;
